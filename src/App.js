d4269192abdefadc237b79f5764bf3aa7e5bcde9
nicko <37054216+nicko-lee@users.noreply.github.com>
2020-06-01 00:06:00 +1000

    Fix initial UI with CSS styling
    Also taking into account responsive design to look decent on web and mobile

--- src/App.js
+++ src/App.js
@@ -27,10 +27,12 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <h1>Welcome to Avalon.</h1>
-        <h2>Click to start new game:</h2>
-        <hr />
-        <p>Deck: {JSON.stringify(this.state.data)}</p>
+        <h1>Welcome to Avalon ⚔️</h1>
+        <h2>Click to start new game 👇🏻</h2>
+        <hr style={horizontalRuleStyle} />
+        <div style={jsonStyle}>
+          <p>Deck: {JSON.stringify(this.state.data)}</p>
+        </div>
         <button type="button" onClick={this.getData}>
           Deal Hand!
         </button>
@@ -38,6 +40,28 @@ export default class App extends React.Component {
     );
   }
 }
+
+var horizontalRuleStyle = {
+  width: "100%",
+  maxWidth: "700px"
+};
+
+// wordWrap makes the JSON text responsive on mobile: https://stackoverflow.com/questions/7641195/how-to-limit-div-width
+// maxWidth kept stuffing things up but when u added marginLeft and right to auto it did the trick to centre the div
+// and keep its max width under control: https://stackoverflow.com/questions/17993471/css-wont-center-div-with-max-width
+var jsonStyle = {
+  width: "100%",
+  backgroundColor: "#BFBFBF",
+  padding: "10px",
+  maxWidth: "700px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginBottom: "15px", // adds some space to next element below
+  marginTop: "15px",
+  textAlign: "center",
+  wordWrap: "break-word",
+  borderRadius: "10px" // this rounds the edges
+};
 
 // Don't need this but keep as reference to loop over list
 const List = props =>
